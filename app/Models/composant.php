58f11cde652872059e988e4ce7403780871e2441
Dr.Pvssy <medchihab651@gmail.com>
2023-05-31 13:30:49 +0200

    ok

---
+++ app/Models/composant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class composant extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/plat.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class plat extends Model
+{
+    use HasFactory;
+}
